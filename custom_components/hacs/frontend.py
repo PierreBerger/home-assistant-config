d12c4264b45b746856487597902adff50b0049f1
Pierre Berger <pierrelinkin@yahoo.fr>
2023-11-28 21:23:51 +0100

    udpate HA

--- custom_components/hacs/frontend.py
+++ custom_components/hacs/frontend.py
@@ -1,17 +1,17 @@
 """"Starting setup task: Frontend"."""
 from __future__ import annotations
 
+import os
 from typing import TYPE_CHECKING
 
-from aiohttp import web
-from homeassistant.components.http import HomeAssistantView
 from homeassistant.core import HomeAssistant, callback
 
-from .const import DOMAIN
-from .hacs_frontend import locate_dir
-from .hacs_frontend.version import VERSION as FE_VERSION
-
-URL_BASE = "/hacsfiles"
+from .const import DOMAIN, URL_BASE
+from .hacs_frontend import VERSION as FE_VERSION, locate_dir
+from .hacs_frontend_experimental import (
+    VERSION as EXPERIMENTAL_FE_VERSION,
+    locate_dir as experimental_locate_dir,
+)
 
 if TYPE_CHECKING:
     from .base import HacsBase
@@ -21,15 +21,22 @@ if TYPE_CHECKING:
 def async_register_frontend(hass: HomeAssistant, hacs: HacsBase) -> None:
     """Register the frontend."""
 
-    # Register themes
-    hass.http.register_static_path(f"{URL_BASE}/themes", hass.config.path("themes"))
+    # Setup themes endpoint if needed
+    hacs.async_setup_frontend_endpoint_themes()
 
     # Register frontend
-    if hacs.configuration.frontend_repo_url:
+    if hacs.configuration.dev and (frontend_path := os.getenv("HACS_FRONTEND_DIR")):
         hacs.log.warning(
             "<HacsFrontend> Frontend development mode enabled. Do not run in production!"
         )
-        hass.http.register_view(HacsFrontendDev())
+        hass.http.register_static_path(
+            f"{URL_BASE}/frontend", f"{frontend_path}/hacs_frontend", cache_headers=False
+        )
+    elif hacs.configuration.experimental:
+        hacs.log.info("<HacsFrontend> Using experimental frontend")
+        hass.http.register_static_path(
+            f"{URL_BASE}/frontend", experimental_locate_dir(), cache_headers=False
+        )
     else:
         #
         hass.http.register_static_path(f"{URL_BASE}/frontend", locate_dir(), cache_headers=False)
@@ -42,21 +49,9 @@ def async_register_frontend(hass: HomeAssistant, hacs: HacsBase) -> None:
         hass.data["frontend_extra_module_url"] = set()
     hass.data["frontend_extra_module_url"].add(f"{URL_BASE}/iconset.js")
 
-    # Register www/community for all other files
-    use_cache = hacs.core.lovelace_mode == "storage"
-    hacs.log.info(
-        "<HacsFrontend> %s mode, cache for /hacsfiles/: %s",
-        hacs.core.lovelace_mode,
-        use_cache,
+    hacs.frontend_version = (
+        FE_VERSION if not hacs.configuration.experimental else EXPERIMENTAL_FE_VERSION
     )
-
-    hass.http.register_static_path(
-        URL_BASE,
-        hass.config.path("www/community"),
-        cache_headers=use_cache,
-    )
-
-    hacs.frontend_version = FE_VERSION
 
     # Add to sidepanel if needed
     if DOMAIN not in hass.data.get("frontend_panels", {}):
@@ -70,28 +65,11 @@ def async_register_frontend(hass: HomeAssistant, hacs: HacsBase) -> None:
                     "name": "hacs-frontend",
                     "embed_iframe": True,
                     "trust_external": False,
-                    "js_url": f"/hacsfiles/frontend/entrypoint.js?hacstag={FE_VERSION}",
+                    "js_url": f"/hacsfiles/frontend/entrypoint.js?hacstag={hacs.frontend_version}",
                 }
             },
             require_admin=True,
         )
 
-
-class HacsFrontendDev(HomeAssistantView):
-    """Dev View Class for HACS."""
-
-    requires_auth = False
-    name = "hacs_files:frontend"
-    url = r"/hacsfiles/frontend/{requested_file:.+}"
-
-    async def get(self, request, requested_file):  # pylint: disable=unused-argument
-        """Handle HACS Web requests."""
-        hacs: HacsBase = request.app["hass"].data.get(DOMAIN)
-        requested = requested_file.split("/")[-1]
-        request = await hacs.session.get(f"{hacs.configuration.frontend_repo_url}/{requested}")
-        if request.status == 200:
-            result = await request.read()
-            response = web.Response(body=result)
-            response.headers["Content-Type"] = "application/javascript"
-
-            return response
+    # Setup plugin endpoint if needed
+    hacs.async_setup_frontend_endpoint_plugin()
